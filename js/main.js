f010932b900588ef9171482c22e42fe00effce6a
Olivier_Luethy <olivier.luethy@gmx.net>
2022-05-01 10:09:04 +0200

    Replay button functional

--- js/main.js
+++ js/main.js
@@ -12,6 +12,10 @@ var attempts = 1;
 var rightPosition = 0;
 /* Variable for not right position but in array */
 var notRightPosition = 0;
+/* Modal when you have won */
+var winmodal = document.getElementById("youHaveWon");
+/* Modal when you have lost */
+var lostmodal = document.getElementById("youHaveLost");
 /* All the colors */
 var colors = ["pink", "red", "violet", "blue", "yellow", "black"];
 /* placeholder */
@@ -170,11 +174,13 @@ document.getElementById("circle-options").addEventListener("click", (e) => {
                 if (rightPosition == 4) {
                     console.log("Finally Rightposition: " + rightPosition, "in the code but not right position: " + notRightPosition);
                     rateing(attempts, rightPosition, notRightPosition);
+                    winmodal.style.display = "block";
                     won = true;
                 } else {
                     if (attempts == 7) {
                         rateing(attempts, rightPosition, notRightPosition);
                         showResult();
+                        lostmodal.style.display = "block";
                     } else {
                         rateing(attempts, rightPosition, notRightPosition);
                         console.log("Finally Rightposition: " + rightPosition, "in the code but not right position: " + notRightPosition);
@@ -243,55 +249,33 @@ document.getElementById("delete").addEventListener("click", function() {
     }
 })
 
-/* Modal when you have won */
-var modal = document.getElementById("youHaveWon");
-
-// Get the button that opens the modal
-var btn = document.getElementById("information");
-
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+var winspan = document.getElementById("have_won_close");
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+winspan.onclick = function() {
+    winmodal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == winmodal) {
+        winmodal.style.display = "none";
     }
 }
 
-/* Modal when you have lost */
-var modal = document.getElementById("youHaveLost");
-
-// Get the button that opens the modal
-var btn = document.getElementById("information");
-
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+var lostspan = document.getElementById("have_lost_close");
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+lostspan.onclick = function() {
+    lostmodal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == lostmodal) {
+        lostmodal.style.display = "none";
     }
 }
 
@@ -320,4 +304,8 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+function replay() {
+    window.location = window.location;
 }
